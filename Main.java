2a31e0374e658e1ac1ec5da362f76269de4ff548
JakubCzyrek <101180591+JakubCzyrek@users.noreply.github.com>
2022-03-22 09:53:36 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("A");
+    System.out.println("e");
+      System.out.println("l");
+      System.out.println("l");
+        System.out.println("b");
+          System.out.println("W");
+            System.out.println("b");
+              System.out.println("r");
+                System.out.println("l");
+                  System.out.println("d");
+  }
+}
